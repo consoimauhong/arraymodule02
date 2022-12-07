5a7259f3e0c2ebb73d5ee4ac244013c663ef8152
Your Name <you@example.com>
2022-12-07 13:40:09 +0700

    asd

---
+++ mang/src/demo.java
@@ -0,0 +1,11 @@
+public class demo {
+    public static void main(String[] args) {
+//        System.out.println("Enter " + matrix.length + " rows and " +
+//                matrix[0].length + " columns: ");
+//        for (int row = 0; row < matrix.length; row++) {
+//            for (int column = 0; column < matrix[row].length; column++) {
+//                matrix[row][column] = input.nextInt();
+//            }
+//        }
+    }
+}
